82452c67bb99be792f56f80992f5f472681bbaf0
kingiw <kingiw@hotmail.com>
2020-06-10 16:50:51 +0800

    fix a bug of glob matcher in prettier

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 
 interface BaseResponse {
-  message: string;
-  data: any;
+    message: string;
+    data: any;
 }
 
 export class Enforcer {
-  private profiles = '';
-  private endpoint: string;
+    private profiles = '';
+    private endpoint: string;
 
-  constructor(endpoint: string) {
-    this.endpoint = endpoint;
-  }
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
 
-  /**
-   * Get the authority of a given user from Casbin core
-   */
-  private async getUserProfiles(user: string): Promise<void> {
-    throw new Error("Not implemented");
-  }
+    /**
+     * Get the authority of a given user from Casbin core
+     */
+    private async getUserProfiles(user: string): Promise<void> {
+        throw new Error('Not implemented');
+    }
 
-  /**
-   * To enforce if the user have the authority to perform `act` on `obj`
-   */
-  public async enforce(obj: string, act: string): Promise<void> {
-    throw new Error("Not implemented");
-  }
+    /**
+     * To enforce if the user have the authority to perform `act` on `obj`
+     */
+    public async enforce(obj: string, act: string): Promise<void> {
+        throw new Error('Not implemented');
+    }
 
-  public async getData(): Promise<any> {
-    const resp = await axios.get<BaseResponse>(this.endpoint);
-    return resp.data.data;
-  }
+    public async getData(): Promise<any> {
+        const resp = await axios.get<BaseResponse>(this.endpoint);
+        return resp.data.data;
+    }
 }
