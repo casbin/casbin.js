ea67aa8d558061e8b37b8ae4e464990085cf507d
kingiw <kingiw@hotmail.com>
2020-07-01 14:00:12 +0800

    add test suites for can, cannot, canAny, canAll

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -19,3 +19,28 @@ test('Mock functions', async () => {
 
     // expect(mockedAxios.get('http://localhost:4000/api/casbin?casbin_subject=alice')).toMatchObject(respObj);
 });
+
+test('Authorizer', () => {
+    const profilesObj = {
+        read: ['data1', 'data2', 'data3'],
+        write: ['data1']
+    }
+    const authorizer = new Authorizer();
+    authorizer.setProfiles(JSON.stringify(profilesObj));
+    // can
+    expect(authorizer.can('read', 'data1')).toBe(true);
+    expect(authorizer.can('read', 'data4')).toBe(false);
+    expect(authorizer.can('write', 'data1')).toBe(true);
+    // cannot
+    expect(authorizer.cannot('read', 'data4')).toBe(true);
+    expect(authorizer.cannot('read', 'data3')).toBe(false);
+    // canAll
+    expect(authorizer.canAll('read', ['data1', 'data2'])).toBe(true);
+    expect(authorizer.canAll('write', ['data1', 'data2'])).toBe(false);
+    expect(authorizer.canAll('read', ['data1', 'data2', 'data4'])).toBe(false);
+    // canAny
+    expect(authorizer.canAny('read', ['data1', 'data2'])).toBe(true);
+    expect(authorizer.canAny('write', ['data1', 'data2'])).toBe(true);
+    expect(authorizer.canAny('read', ['data1', 'data2', 'data4'])).toBe(true);
+    expect(authorizer.canAny('read', ['data4'])).toBe(false);
+})
