78d99d62a4fe5174ef939e46fc17eb256f64d419
kingiw <kingiw@hotmail.com>
2020-07-06 15:02:31 +0800

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -22,4 +23,7 @@ module.exports = {
     filename: 'casbin.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 };
