b210c71fc8f8c90671a95e4807534328ad0e00d3
kingiw <kingiw@hotmail.com>
2020-07-07 20:33:45 +0800

    sort arrays when comparison

--- src/__test__/permission.test.ts
+++ src/__test__/permission.test.ts
@@ -6,14 +6,8 @@ describe('Permission unit test', () => {
     }
     const permission = new Permission();
 
-    // From https://stackoverflow.com/a/57428906/7409994
-    const expectArrayEquivalence = <T>(actual: T[], expected: T[]) => {
-        expect(actual).toEqual(expect.arrayContaining(expected));
-        expect(expected).toEqual(expect.arrayContaining(actual));
-      };
-    
     test('Load permission from JSON string', () => {
-        const s = JSON.stringify(policyExample);\
+        const s = JSON.stringify(policyExample);
         permission.loadFromString(s);
         const t = permission.getPermissionString();
         expect(JSON.parse(t)).toMatchObject(policyExample);
@@ -21,12 +15,15 @@ describe('Permission unit test', () => {
 
     test('Get targets from an action', () => {
         permission.loadFromString(JSON.stringify(policyExample));
-        expectArrayEquivalence<string>(permission.getTargetsFromAction('read'), policyExample['read']);
-        expectArrayEquivalence<string>(permission.getTargetsFromAction('write'), policyExample['write']);
-        // ?: The following implementation doesn't work. Help wanted.
-        // ['read', 'write'].forEach((item :string) => {
-        //     expectArrayEquivalence<string>(permission.getTargetsFromAction(item), policyExample[item]);
-        // })
+
+        let expected = policyExample['read'];
+        let actual = permission.getTargetsFromAction('read');
+        expect(actual.sort()).toEqual(expected.sort());
+
+        expected = policyExample['write'];
+        actual = permission.getTargetsFromAction('write');
+        expect(actual.sort()).toEqual(expected.sort());
+
     })
 
     test('Get action-object mapping', () => {
