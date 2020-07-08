7d94b32a76554b6ae41527767a737a63b7f93b3d
kingiw <kingiw@hotmail.com>
2020-07-08 23:59:40 +0800

    update webpack config for dev and prod modes

---
+++ webpack.base.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    library: 'casbin',
+    libraryTarget: 'umd',
+    filename: 'casbin.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+};
