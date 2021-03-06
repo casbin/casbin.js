bf496eeffe30691b12f4d0d8bee1257934c364dc
Zxilly <zhouxinyu1001@gmail.com>
2021-03-06 23:32:52 +0800

    remove unused import
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Cookies from 'js-cookie'
 import { Authorizer } from '../index';
 import { basicModelStr } from './models';
 import { basicPolicies } from './policies';
